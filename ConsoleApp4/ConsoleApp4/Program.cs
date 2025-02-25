426b45b804689b90e81b496519d5ad0f4a4ae2aa
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-02-25 15:40:11 +0900

    오후 강의 3
    콘솔좌표

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
+            Console.CursorVisible = false;
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            Console.Write("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+            Console.SetCursorPosition(0, 1);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 2);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 3);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 4);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 5);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 6);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 7);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 8);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 9);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 10);
+            Console.Write("┃                             대장장이키우기                                   ┃");
+            Console.SetCursorPosition(0, 11);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 12);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 13);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 14);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 15);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 16);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 17);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 18);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 19);
+            Console.Write("┃                                                                              ┃");
+            Console.SetCursorPosition(0, 20);
+            Console.Write("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┚");
+
+            Thread.Sleep(3000);
+            
+            for(int x = 0; x<30; x++)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 10);
+                Console.Write("◎");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
